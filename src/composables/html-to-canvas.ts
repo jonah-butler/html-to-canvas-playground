7713b75f37b24f84a232264b3c09d058352d6c3d
jonah-butler <jonahbutler6@gmail.com>
2023-12-15 19:37:43 -0500

    first commit

--- src/composables/html-to-canvas.ts
+++ src/composables/html-to-canvas.ts
@@ -13,24 +13,30 @@ const convertToSVG = (
   e: ElementJSON,
   canvasContext: CanvasRenderingContext2D
 ): void => {
-  console.log(e.reference!);
   e.reference!.setAttribute("xmlns", "http://www.w3.org/1999/xhtml");
+
   const height = e.reference!.clientHeight;
   const width = e.reference!.clientWidth;
+
   let data = "";
   let url = "";
   let image = new Image();
+
   if (e.type === "image") {
+    // move all this into own function
     image.width = width;
     image.height = height;
     url = e.src as string;
   } else {
+    // move into own function
     data = `<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
-    <foreignObject width="100%" height="100%">
-      ${removeTransformProperty(e.reference!.outerHTML)}
-    </foreignObject>
-  </svg>`;
+              <foreignObject width="100%" height="100%">
+                ${removeTransformProperty(e.reference!.outerHTML)}
+              </foreignObject>
+            </svg>`;
+
     const svg = new Blob([data], { type: "image/svg+xml;charset=utf-8" });
+
     url = window.URL.createObjectURL(svg);
   }
 
